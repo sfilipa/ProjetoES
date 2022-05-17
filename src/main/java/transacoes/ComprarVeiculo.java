69c75ea9772dc43fb362f2544018ca8f3d315ad5
Micael Marques <2201743@my.ipleiria.pt>
2022-05-17 19:08:57 +0100

    criar ComprarVeiculo -> falta mudar nome de labels

---
+++ src/main/java/transacoes/ComprarVeiculo.java
@@ -0,0 +1,27 @@
+package transacoes;
+
+import javax.swing.*;
+
+public class ComprarVeiculo {
+    private JPanel painelPrincipal;
+    private JButton comprarButton;
+    private JButton cancelarButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField14;
+    private JRadioButton gasolinaRadioButton;
+    private JRadioButton dieselRadioButton;
+    private JRadioButton eletricoRadioButton;
+    private JRadioButton automaticaRadioButton;
+    private JRadioButton manualRadioButton;
+    private JRadioButton fronteiraRadioButton;
+    private JRadioButton trazeiraRadioButton;
+}
