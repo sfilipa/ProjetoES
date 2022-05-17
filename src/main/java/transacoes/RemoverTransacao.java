2ab2614d0df2966ee401bfe9672e55bed00cc3ee
Micael Marques <2201743@my.ipleiria.pt>
2022-05-17 21:46:35 +0100

    acrescentar o RemoverTransacao

---
+++ src/main/java/transacoes/RemoverTransacao.java
@@ -0,0 +1,24 @@
+package transacoes;
+
+import javax.swing.*;
+
+public class RemoverTransacao {
+    private JPanel painelPrincipal;
+    private JLabel removerTransacao;
+    private JComboBox transacaoRemoverBox;
+    private JRadioButton dinheiroRadioButton;
+    private JRadioButton multibancoRadioButton;
+    private JButton removerTransacaoButton;
+    private JButton cancelarButton;
+    private JLabel transacaoRemover;
+    private JLabel infoTransacao;
+    private JLabel valor;
+    private JLabel matricula;
+    private JLabel dataTransacao;
+    private JLabel horaTransacao;
+    private JLabel tipoTransacao;
+    private JLabel valorText;
+    private JLabel matriculaText;
+    private JLabel dataTransacaoText;
+    private JLabel horaTransacaoText;
+}
