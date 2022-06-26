15dca84f3e584d68ac1ca1de644270a59f6c2c80
RicardoFilipe00 <59094987+RicardoFilipe00@users.noreply.github.com>
2022-06-26 03:11:53 +0100

    Estatisticas

--- src/main/java/vista/estatisticas/EstatisticasPagina3.java
+++ src/main/java/vista/estatisticas/EstatisticasPagina3.java
@@ -1,19 +1,26 @@
 package vista.estatisticas;
 
 
+import modelo.DadosAplicacao;
+import modelo.Veiculo;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EstatisticasPagina3 extends  JFrame{
     private JPanel painelPrincipal;
     private JList list1;
     private JList list2;
     private JButton voltarButton;
+    private JList listaModelo;
     private JList list3;
 
     public EstatisticasPagina3(){
         setContentPane(painelPrincipal);
+        atualizarListaModelos();
         pack();
 
         btnVoltarActionPerformed();
@@ -27,6 +34,19 @@ public class EstatisticasPagina3 extends  JFrame{
                 new PaginaInicialEstatisticas().setVisible(true);
             }
         });
+    }
+
+    private void atualizarListaModelos() {
+        List<Veiculo> veiculos = new ArrayList<>();
+        DadosAplicacao dadosAplicacao = DadosAplicacao.INSTANCE;
+        veiculos = dadosAplicacao.getVeiculos();
+        List<String> modelos = new ArrayList<>();
+        DefaultListModel<String> model = new DefaultListModel<>();
+        for (Veiculo veiculo : veiculos) {
+            model.addElement(veiculo.getModelo());
+        }
+        listaModelo.setModel(model);
+        list1.setModel(model);
     }
 
     public static void main(String[] args) {
